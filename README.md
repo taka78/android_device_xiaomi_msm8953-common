a5086044c8236740b1d0927c88fac9b70f99b9aa
Alkiyanya <1376106928@qq.com>
2018-03-23 14:11:26 +0800

    msm8953-common: Initial common tree push

---
+++ README.md
@@ -0,0 +1,15 @@
+Copyright (C) 2017-2018 - The LineageOS Project
+
+Common device tree for Xiaomi MSM8953 based devices
+==============
+
+> 注意！只支持以下打钩的设备 
+> Note! Only supports the following ticked devices
+
+## 支持的设备 Supported devices
+- [ ] 红米4高配 Markw
+- [ ] 红米Note4X Mido
+- [ ] 小米MAX2 Oxygen
+- [x] 红米5Plus Vince
+- [x] 小米5X Tiffany
+- [x] 小米A1 Tissot
---
+++ bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_device[PROPERTY_VALUE_MAX];
+    property_get("ro.product.device", product_device, "");
+
+    if (strstr(product_device, "vince"))
+        return "Xiaomi Redmi 5 Plus";
+    if (strstr(product_device, "tiffany"))
+        return "Xiaomi MI 5X";
+    if (strstr(product_device, "tissot"))
+        return "Xiaomi MI A1";
+
+    // Fallback to ro.product.model
+    return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS    7
+#define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
+#define BT_CLEAN_TURN_ON_DISABLED 1
+
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+#endif
